0b762df32180827bd8c4f5d44c491431ec2d5f17
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2023-01-17 04:44:10 +0000

    TRIM script to 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
-
 #this script cURL an arg and returns the size of response
-
-#check if a URL is provided as arg
-if [ $# -eq 0 ]
-then
-	echo "Please provide a URL arg"
-	exit 1
-fi
-
-#cURL operation
-response=$(curl -s -o /dev/null -w %{size_download} $1)
-
-# check for empty response
-if [ -z "$response" ]
-then
-	echo "Null response from server"
-	exit 1
-fi
-
-# Print size of response body
-echo "$response"
+echo $(curl -s -o /dev/null -w %{size_download} $1)
